61966bbc6683277019873866bd20a397b78eb270
BartMiki <bartmiki97@gmail.com>
2018-03-17 22:30:37 +0100

    Added main window format

--- ChatBot_Client/MainWindow.xaml.cs
+++ ChatBot_Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,30 @@ namespace ChatBot_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly ObservableCollection<string> messages = new ObservableCollection<string>();
+
         public MainWindow()
         {
+            DataContext = messages;
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Text != String.Empty)
+            {
+                messages.Add($"You: {MessageBox.Text}");
+                MessageBox.Text = String.Empty;
+            }
+        }
+
+        private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && MessageBox.Text != String.Empty)
+            {
+                messages.Add($"You: {MessageBox.Text}");
+                MessageBox.Text = String.Empty;
+            }
         }
     }
 }
